c9edacf3ba2f68786abe9c6401b2663852350da1
BennysonSamuel <bennyson64@gmail.com>
2026-02-11 08:17:51 +0530

    no fonts

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,18 +1,18 @@
 
-import { Geist, Geist_Mono } from "next/font/google"
+// import { Geist, Geist_Mono } from "next/font/google"
 
 import "@workspace/ui/globals.css"
 import { Providers } from "@/components/providers"
 
-const fontSans = Geist({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
+// const fontSans = Geist({
+//   subsets: ["latin"],
+//   variable: "--font-sans",
+// })
 
-const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-})
+// const fontMono = Geist_Mono({
+//   subsets: ["latin"],
+//   variable: "--font-mono",
+// })
 export default function RootLayout({
   children,
 }: Readonly<{
