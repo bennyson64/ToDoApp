244fdfe1bf7037eb8a4812e7859ff8af7b219fd0
BennysonSamuel <bennyson64@gmail.com>
2026-02-11 06:54:36 +0530

    restart

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,17 @@
+// import { Geist, Geist_Mono } from "next/font/google";
+
 import "@workspace/ui/globals.css";
 import { Providers } from "@/components/providers";
+
+// const fontSans = Geist({
+//   subsets: ["latin"],
+//   variable: "--font-sans",
+// });
+
+// const fontMono = Geist_Mono({
+//   subsets: ["latin"],
+//   variable: "--font-mono",
+// });
 
 export default function RootLayout({
   children,
